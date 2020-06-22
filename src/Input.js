634174bf1f067784a607645baec290c3207d3892
Andrew <andrewriznykua93@gmail.com>
2020-06-21 22:28:30 -0500

    Add redux props tests to Input

--- src/Input.js
+++ src/Input.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { guessWord } from './actions/index';
+
 class Input extends Component {
   render() {
     const contents = this.props.success 
@@ -30,6 +32,10 @@ class Input extends Component {
 
 const mapStateToProps = ({ success }) => {
   return { success };
+};;
+
+const actions = { 
+  guessWord
 }
 
-export default connect(mapStateToProps)(Input);
+export default connect(mapStateToProps, actions)(Input);
--- src/Input.test.js
+++ src/Input.test.js
@@ -25,50 +25,60 @@ const setup = (initialState = {}) => {
     return wrapper;
     }
 
-    setup();
+setup();
 
-    describe('render', () => {
-      describe('word has not been guessed', () => {
-        let wrapper;
-        beforeEach(() => {
-          const initialState = { success: false };
-          wrapper = setup(initialState);
-        });
-        test('renders component without error', () => {
-          const component = findByTestAttr(wrapper, "component-input");
-          expect(component.length).toBe(1);
-        });
-        test('renders input box', () => {
-          const inputBox = findByTestAttr(wrapper, "input-box");
-          expect(inputBox.length).toBe(1);
-        });
-        test('renders submit button', () => {
-          const submitButton = findByTestAttr(wrapper, "submit-button");
-          expect(submitButton.length).toBe(1);
-        });
-      });
-
-      describe('word has been guessed', () => {
-        let wrapper;
-        beforeEach(() => {
-          const initialState = { success: true };
-          wrapper = setup(initialState);
-        });
-        test('renders component without error', () => {
-          const component = findByTestAttr(wrapper, "component-input");
-          expect(component.length).toBe(1);
-        });
-        test('does not renders input box', () => {
-          const inputBox = findByTestAttr(wrapper, "input-box");
-          expect(inputBox.length).toBe(0);
-        });
-        test('does not renders submit button', () => {
-          const submitButton = findByTestAttr(wrapper, "submit-button");
-          expect(submitButton.length).toBe(0);
-        });
-      });
+describe('render', () => {
+  describe('word has not been guessed', () => {
+    let wrapper;
+    beforeEach(() => {
+      const initialState = { success: false };
+      wrapper = setup(initialState);
     });
-
-    describe('update state', () => {
-
+    test('renders component without error', () => {
+      const component = findByTestAttr(wrapper, "component-input");
+      expect(component.length).toBe(1);
     });
+    test('renders input box', () => {
+      const inputBox = findByTestAttr(wrapper, "input-box");
+      expect(inputBox.length).toBe(1);
+    });
+    test('renders submit button', () => {
+      const submitButton = findByTestAttr(wrapper, "submit-button");
+      expect(submitButton.length).toBe(1);
+    });
+  });
+
+  describe('word has been guessed', () => {
+    let wrapper;
+    beforeEach(() => {
+      const initialState = { success: true };
+      wrapper = setup(initialState);
+    });
+    test('renders component without error', () => {
+      const component = findByTestAttr(wrapper, "component-input");
+      expect(component.length).toBe(1);
+    });
+    test('does not renders input box', () => {
+      const inputBox = findByTestAttr(wrapper, "input-box");
+      expect(inputBox.length).toBe(0);
+    });
+    test('does not renders submit button', () => {
+      const submitButton = findByTestAttr(wrapper, "submit-button");
+      expect(submitButton.length).toBe(0);
+    });
+  });
+});
+
+describe('redux props', () => {
+  test('has success piece of state as prop', () => {
+    const success =true;
+    const wrapper = setup({ success });
+    const successProp = wrapper.instance().props.success;
+    expect(successProp).toBe(success);
+  });
+  test('`guessWord` action creator is a function prop', () => {
+    const wrapper = setup();
+    const guessWordProp = wrapper.instance().props.guessWord;
+    expect(guessWordProp).toBeInstanceOf(Function);
+  });
+});
